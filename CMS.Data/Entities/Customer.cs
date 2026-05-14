9d1cf64c2e342b9c76b4a45141f15c77390774b3
Admin <Admin@D20837>
2026-05-14 10:50:13 +0700

    Add project files.

---
+++ CMS.Data/Entities/Customer.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * ten sinh vien:Truong Quoc Toan
+ * ma sinh vien:2123110015
+ * Ngay lam:14/05/2026
+ * version:1
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Data.Entities
+{
+    // Khách hàng
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Address { get; set; }
+
+        [Required]
+        public string Password { get; set; } // Lưu mật khẩu thô theo yêu cầu tối giản
+
+        public virtual ICollection<Order>? Orders { get; set; }
+    }
+}
---
+++ CMS.Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * ten sinh vien:Truong Quoc Toan
+ * ma sinh vien:2123110015
+ * Ngay lam:14/05/2026
+ * version:1
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên
+    }
+}
+
